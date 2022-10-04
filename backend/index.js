05591d6d7e87e47ebacff418bb9490ce5a89cae0
Surneke <surnee399@gmail.com>
2022-10-04 12:43:08 +0800

    first push

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const data = require("./data.json")
+const cors = require("cors")
+const express = require("express")
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.get("/users" , (req,res) => {
+    console.log(data)
+    res.status(200).send(data)
+
+});
+app.listen(port,()=> {
+    console.log(`Listening on port ${port}`)
+});
